231ab099a3122abc224de66d2e80f516c5d5160c
Varga Norbert <vnorbi02@gmail.com>
2023-02-01 16:24:20 +0100

    kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,20 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Render,
+} from '@nestjs/common';
+import { DataSource, ReturningStatementNotSupportedError } from 'typeorm';
 import { AppService } from './app.service';
-
+import { RegisterDto } from './register.dto';
+import User from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { ChangeUserDto } from './changeUser.dto';
 @Controller()
 export class AppController {
   constructor(
@@ -13,5 +26,55 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Post('/register')
+  register(@Body() registerDto: RegisterDto) {
+    if (
+      !registerDto.email ||
+      !registerDto.password ||
+      !registerDto.passwordAgain
+    ) {
+      throw new BadRequestException('All fields must ');
+    }
+
+    if (!registerDto.email.includes('@')) {
+      throw new BadRequestException('Email must contain an @ character');
+    }
+    if (registerDto.password !== registerDto.passwordAgain) {
+      throw new BadRequestException('The two passwords must match');
+    }
+
+    if (registerDto.password.length < 8) {
+      throw new BadRequestException(
+        'The password must be at least 8 characters long',
+      );
+    }
+
+    const userRepo = this.dataSource.getRepository(User);
+    const user = new User();
+    user.email = registerDto.email;
+    user.password = bcrypt.hash(registerDto.password, 15);
+    userRepo.save(user);
+  }
+  @Patch('users/:id')
+  async patchUser(
+    @Param('id') id: number,
+    @Body() changeUserDto: ChangeUserDto,
+  ) {
+    if (!changeUserDto.email.includes('@')) {
+      throw new BadRequestException('Email must contain an @ character');
+    }
+    if (
+      !changeUserDto.profilePictureUrl.startsWith('http://') &&
+      changeUserDto.profilePictureUrl.startsWith('https://')
+    ) {
+      throw new BadRequestException('Url must start with http or https');
+    }
+    const userRepo = this.dataSource.getRepository(User);
+    const userToChange = await userRepo.findOneBy({ id: id });
+    userToChange.email = changeUserDto.email;
+    userToChange.profilePictureUrl = changeUserDto.profilePictureUrl;
+    userRepo.save(userToChange);
   }
 }
